8fbba238ed561dff7a97b6a672d2642cb4fc36e8
uvafan <uvafan173@gmail.com>
2017-10-25 16:17:56 -0400

    10557

--- graphs/10557.cpp
+++ graphs/10557.cpp
@@ -7,23 +7,13 @@ typedef vector<int> vi;
 
 int V,i,j;
 
-bool conn(vector<vii> adjList, int s){
-    vi dist(V,INF); dist[s]=0;
-    for(i=0;i<V-1;i++)
-        for(int u=0;u<V;u++)
-            for(j=0;j<adjList[u].size();j++){
-                ii v = adjList[u][j];
-                dist[v.first] = min(dist[v.first],dist[u]+v.second);
-            }
-    return dist[V-1]<INF/2;
-}
-
 int main(){
     int N,E,x,y,t;
     while(1){
         cin>>V;
         if(V==-1)break;
         vector<vii> adjList(V,vii(0,ii(0,0)));
+        vector<vi> adjListB(V,vi(0,0));
         vi energies(V,0);
         for(i=0;i<V;i++){
             cin>>x>>y;
@@ -31,32 +21,52 @@ int main(){
             while(y--){
                 cin>>t;
                 adjList[i].push_back(ii(t-1,0));
+                adjListB[t-1].push_back(i);
             }
         }
         for(i=0;i<V;i++){
             for(j=0;j<adjList[i].size();j++){
-                adjList[i][j].second = -energies[adjList[i][j].first];
+                adjList[i][j].second = energies[adjList[i][j].first];
             }
         }
-        vi dist(V,INF); dist[0]=0;
+        vi dist(V,-INF); dist[0]=100;
         for(i=0;i<V-1;i++)
-            for(int u=0;u<V;u++)
+            for(int u=0;u<V;u++){
+                if(dist[u]<0)continue;
                 for(j=0;j<adjList[u].size();j++){
                     ii v = adjList[u][j];
-                    dist[v.first] = min(dist[v.first],dist[u]+v.second);
+                    dist[v.first] = max(dist[v.first],dist[u]+v.second);
                 }
+            }
         bool poss = false;
+        //cout<<"b"<<endl;
+        vi conn(V,0);
+        queue<int> q;
+        q.push(V-1);
+        conn[V-1]=1;
+        while(!q.empty()){
+            int u = q.front(); q.pop();
+            for(i=0;i<adjListB[u].size();i++){
+                int v = adjListB[u][i];
+                if(!conn[v]){
+                    conn[v] = 1;
+                    q.push(v);
+                }
+            }
+        }
         for(i=0;i<V;i++){
             if(poss)break;
             for(j=0;j<adjList[i].size();j++){
                 ii v = adjList[i][j];
-                if(dist[v.first]<INF/2&&conn(adjList,v.first)&&(dist[i]+v.second<dist[v.first])){
+                if(dist[i]>0&&(dist[i]+v.second>dist[v.first])&&conn[v.first]){
+                    //cout<<"di = "<<dist[i] << " od = " << dist[v.first] << " nd = "<<dist[i]+v.second<<" ";
                     poss = true;
                     break;
                 }
             }
         }
-        if(poss||dist[V-1]<100)cout<<"winnable"<<endl;
+        //if(poss)cout<<"poss ";
+        if(poss||dist[V-1]>0)cout<<"winnable"<<endl;
         else cout<<"hopeless"<<endl;
     }
     return 0;
