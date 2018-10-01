6a6e6b2070d021f36ae5c6f90c3429d655815708
uvafan <eli.d.lifland@gmail.com>
2018-09-30 20:09:45 -0400

    updated template

--- template.cpp
+++ template.cpp
@@ -1,11 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 typedef pair<int,int> ii;
 typedef vector<ii> vii;
 typedef vector<int> vi;
+typedef set<int> si;
 
+#define repr(i,l,r) for(int i=(l), _##i=(r); i<_##i; ++i)
+#define d(arg) do {cerr << #arg << ": "; cerr << arg; cerr << endl;} while(0)
 #define pb push_back
-#define INF 1e9
+#define INF (int)1e9
+#define EPS 1e-9
 
 int N,i,j;
 int main(){
