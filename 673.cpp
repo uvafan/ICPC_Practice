e9e732f6ad225678131718938a908bb486ef31da
uvafan <eli.d.lifland@gmail.com>
2018-11-02 19:15:49 -0400

    673 acc

---
+++ 673.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+
+#define repr(i,l,r) for(int i=(l), _##i=(r); i<_##i; ++i)
+#define d(arg) do {cerr << #arg << ": "; cerr << arg; cerr << endl;} while(0)
+#define pb push_back
+#define INF (int)1e9
+#define EPS 1e-9
+
+int main(){
+    int N;
+    cin>>N;
+    while(N--){
+        vector<char> s;
+        string line;
+        cin>>line;
+        string out = "Yes";
+        for(char c: line){
+            if(c=='('||c=='[')
+                s.pb(c);
+            else if(c==']'){
+                if(!s.size()||s[s.size()-1]!='['){
+                    out="No";
+                    break;
+                }
+                s.pop_back();
+            }
+            else{
+                if(!s.size()||s[s.size()-1]!='('){
+                    out="No";
+                    break;
+                }
+                s.pop_back();
+            }
+        }
+        cout<<out<<endl;
+    }
+    return 0;
+}
